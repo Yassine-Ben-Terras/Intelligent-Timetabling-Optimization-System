72a46f9290cc7158c3a0a72825bb8f90a149e98d
Yassine-Ben-Terras <yassine.benterras@gmail.com>
2025-05-05 12:00:00 +0000

    feat: add base message response payload structure

---
+++ Back-end/src/main/java/com/Session/EmploiTempsV1/emploi/payload/response/MessageResponse.java
@@ -0,0 +1,13 @@
+package com.Session.EmploiTempsV1.emploi.payload.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
