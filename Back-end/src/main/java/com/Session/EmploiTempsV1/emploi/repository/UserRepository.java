c63dfbf098f85c8ed33e6fd43697bed00fc2027c
Yassine-Ben-Terras <yassine.benterras@gmail.com>
2025-05-07 12:00:00 +0000

    feat: implement repositories for User and Role entities

---
+++ Back-end/src/main/java/com/Session/EmploiTempsV1/emploi/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.Session.EmploiTempsV1.emploi.repository;
+
+
+import com.Session.EmploiTempsV1.emploi.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
